fb60c76750db4ef33e4a401152f06fc0ad30258c
Yaboba <tsitov05@gmail.com>
2023-11-08 02:19:34 +0300

    add grid

---
+++ src/grid.py
@@ -0,0 +1,45 @@
+def curl_E(E):
+    pass
+
+def curl_H(H):
+    pass
+
+class Grid:
+    def __init__(self,):
+        pass
+    
+    def _handle_distance(self,):
+        pass
+
+    def _handle_time(self,):
+        pass
+
+    @property
+    def x(self):
+        pass
+
+    @property
+    def y(self):
+        pass
+
+    @property
+    def z(self):
+        pass
+
+    def run(self,):
+        pass
+
+    def update_E(self,):
+        pass
+
+    def update_H(self, ):
+        pass
+
+    def add_source(self,):
+        pass
+
+    def add_object(self, ):
+        pass
+
+    def add_detector(self, ):
+        pass
